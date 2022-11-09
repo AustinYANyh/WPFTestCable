adeb940806adbca4c33cf1e720a1ac33d76447db
AustinYanyh <1241528539@qq.com>
2022-11-09 10:09:25 +0800

    增加Window的cs文件

---
+++ WpfTestProject/CableReport/Window/ExportMain.xaml.cs
@@ -0,0 +1,94 @@
+﻿// ============================================================================================
+// *   Copyright (C) 2022 All rights reserved.
+// *
+// *   文件名称：ExportMain.xaml.cs
+// *   创 建 者：yanyunhao
+// *   创建日期：2022年11月07日 11:16
+// *   描    述：
+// ==============================================================================================
+
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using HW.JD.CableReport;
+using HW.JD.CableReport.Model;
+using WpfTestProject.CableReport.UserControl;
+
+namespace WpfTestProject.CableReport.Window
+{
+    public partial class ExportMain : System.Windows.Window
+    {
+        public ExportMain()
+        {
+            InitializeComponent();
+            
+            DataInfos = new ObservableCollection<ExportDataGridInfo>
+            {
+                new ExportDataGridInfo(){Number = "1",CableNumber = "xx", CableInfo = "123", CableSize = "xx-xx-yy", EndPointNumber = "999", EndPointName = "End", StartPointNumber = "111",StartPointName = "Start"},
+                new ExportDataGridInfo(){Number = "2",CableNumber = "YY", CableInfo = "123", CableSize = "xx-xx-yy", EndPointNumber = "999", EndPointName = "End", StartPointNumber = "111",StartPointName = "Start"},
+                new ExportDataGridInfo(){Number = "3",CableNumber = "ZZ", CableInfo = "123", CableSize = "xx-xx-yy", EndPointNumber = "999", EndPointName = "End", StartPointNumber = "111",StartPointName = "Start"},
+                new ExportDataGridInfo(){Number = "4",CableNumber = "aa", CableInfo = "123", CableSize = "xx-xx-yy", EndPointNumber = "999", EndPointName = "End", StartPointNumber = "111",StartPointName = "Start"},
+            };
+            this.DataContext = this;
+        }
+
+        public ObservableCollection<ExportDataGridInfo> DataInfos { get; set; } = new ObservableCollection<ExportDataGridInfo>();
+        
+        /// <summary>
+        /// 导入清册
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ExportFromExcelBtnOnClick(object sender, RoutedEventArgs e)
+        {
+            List<DataGridInfo> infos = new List<DataGridInfo>();
+            foreach (DataGridColumn dataGridColumn in ExportResultDataGrid.Columns.ToList().OrderBy(x=>x.DisplayIndex))
+            {
+                if (dataGridColumn.Header is string headStr)
+                {
+                    string titleName = "起始行";
+                    InfoType infoType = InfoType.None;
+                    if (infos.Exists(x => x.TitleName.Equals(titleName)))
+                    {
+                        titleName = "新增列";
+                        infoType = InfoType.NewCol;
+                    }
+                    infos.Add(new DataGridInfo(){TitleName = titleName,IsNeedRedStar = false,ExcelIndex = 0,DataType = infoType});
+                }
+                else if (dataGridColumn.Header is TextBox textBox)
+                {
+                    infos.Add(new DataGridInfo() { TitleName = textBox.Text.Trim(), IsNeedRedStar = false, ExcelIndex = 0 , DataType = GlobalProperty.GetDataType(textBox)});
+                }
+                else if(dataGridColumn.Header is TextBlockWithRedStar textBlockWithRedStar)
+                {
+                    infos.Add(new DataGridInfo() { TitleName = textBlockWithRedStar.Header, IsNeedRedStar = true, ExcelIndex = 0 ,DataType = GlobalProperty.GetDataType(textBlockWithRedStar)});
+                }
+            }
+            ExportSecond second = new ExportSecond(infos);
+            second.Owner = this;
+            second.ShowDialog();
+            
+            DataInfos.Clear();
+            List<ExportDataGridInfo> exportResult = second.GetExportResult();
+            exportResult.ForEach(x=>DataInfos.Add(x));
+        }
+
+        /// <summary>
+        /// 增加列
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void AddColumnBtn_OnClick(object sender, RoutedEventArgs e)
+        {
+            int newColumnIndex = DataInfos.First().NewColumnIndex + 1;
+            DataGridTextColumn column = new DataGridTextColumn(){Header = "新增列",Binding = new Binding($"NewColumnData[{newColumnIndex}]")};
+            ExportResultDataGrid.Columns.Add(column);
+
+            DataInfos.First().NewColumnIndex += 1;
+        }
+    }
+}
---
+++ WpfTestProject/CableReport/Window/ExportSecond.xaml.cs
@@ -0,0 +1,138 @@
+﻿// ============================================================================================
+// *   Copyright (C) 2022 All rights reserved.
+// *
+// *   文件名称：ExportSecond.xaml.cs
+// *   创 建 者：yanyunhao
+// *   创建日期：2022年11月07日 15:32
+// *   描    述：
+// ==============================================================================================
+
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Input;
+using HW.JD.CableReport.Model;
+using HW.JD.CableReport.ZHelper;
+using Microsoft.Office.Interop.Excel;
+
+namespace WpfTestProject.CableReport.Window
+{
+    public partial class ExportSecond : System.Windows.Window
+    {
+        public ExcelHelper ExcelHelper { get; set; }= new ExcelHelper();
+        public ExportSecond(List<DataGridInfo> infos)
+        {
+            InitializeComponent();
+
+            SheetData = new ObservableCollection<SheetInfo>
+            {
+                new SheetInfo() {IsChecked = true, Content = "电力电缆表"},
+                new SheetInfo() {IsChecked = false, Content = "电缆标注"}
+            };
+
+            DataGridData = new ObservableCollection<DataGridInfo>(infos);
+            UpdateExcelColumnInDataGrid();
+
+            DataContext = this;
+        }
+
+        private void UpdateExcelColumnInDataGrid()
+        {
+            foreach (DataGridInfo dataGridInfo in DataGridData)
+            {
+                dataGridInfo.ExcelIndex = 0;
+                dataGridInfo.ExcelColumns = new List<string>(ExcelHelper.GetColumnNames());
+            }
+        }
+
+        // private List<string> columns = new List<string>() {"A", "B", "C"};
+
+        public ObservableCollection<SheetInfo> SheetData { get; set; } = new ObservableCollection<SheetInfo>();
+        public ObservableCollection<DataGridInfo> DataGridData { get; set; } = new ObservableCollection<DataGridInfo>();
+
+        private List<ExportDataGridInfo> ExportResult = new List<ExportDataGridInfo>();
+
+        public List<ExportDataGridInfo> GetExportResult() => ExportResult;
+
+        private void ExportBtnOnClick(object sender, RoutedEventArgs e)
+        {
+            if(!ExcelHelper.GetExcelData())return;
+            
+            SheetData.Clear();
+            foreach (string workSheetName in ExcelHelper.GetWorkSheetNames())
+            {
+                SheetData.Add(new SheetInfo() {IsChecked = false, Content = workSheetName});
+            }
+            UpdateExcelColumnInDataGrid();
+        }
+
+        private void ExportToMainBtnOnClick(object sender, RoutedEventArgs e)
+        {
+            if (!SheetData.Any(x => x.IsChecked))
+            {
+                MessageBox.Show("不选工作簿是导不了的");
+                return;
+            }
+
+            ExportResult = new List<ExportDataGridInfo>();
+
+            //ToDo 起始行开始读取
+
+            foreach (SheetInfo sheetInfo in SheetData)
+            {
+                if (!sheetInfo.IsChecked) continue;
+
+                Dictionary<string,List<ExcelData>> dictionary = ExcelHelper.GetData();
+                List<ExcelData> excelData = dictionary[sheetInfo.Content];
+                
+                //按行读取
+                foreach (ExcelData data in excelData)
+                {
+                    for (var i = 0; i < data.DataRows.Count; i++)
+                    {
+                        var dataDataRow = data.DataRows[i];
+                        ExportDataGridInfo resultInfo = new ExportDataGridInfo() {Number = (i + 1).ToString()};
+                        //按照选择的列读取
+                        foreach (DataGridInfo dataGridInfo in DataGridData)
+                        {
+                            //没有这一列
+                            if (!data.ColumnNames.Contains(dataGridInfo.DisplayCol) || dataGridInfo.DataType == InfoType.None)
+                            {
+                                continue;
+                            }
+
+                            string resultStr = Convert.ToString(dataDataRow[dataGridInfo.DisplayCol]);
+
+                            //列数据有误
+                            if (string.IsNullOrEmpty(resultStr)) continue;
+
+                            //新增列添加到集合中
+                            if (dataGridInfo.DataType == InfoType.NewCol)
+                            {
+                                resultInfo.NewColumnData.Add(resultStr);
+                                resultInfo.NewColumnIndex += 1;
+                                continue;
+                            }
+
+                            if (dataGridInfo.DataType == InfoType.CableInfo) resultInfo.CableInfo = resultStr;
+                            if (dataGridInfo.DataType == InfoType.CableNumber) resultInfo.CableNumber = resultStr;
+                            if (dataGridInfo.DataType == InfoType.CableSize) resultInfo.CableSize = resultStr;
+                            if (dataGridInfo.DataType == InfoType.StartPointName) resultInfo.StartPointName = resultStr;
+                            if (dataGridInfo.DataType == InfoType.EndPointName) resultInfo.EndPointName = resultStr;
+                            if (dataGridInfo.DataType == InfoType.StartPointNumber) resultInfo.StartPointNumber = resultStr;
+                            if (dataGridInfo.DataType == InfoType.EndPointNumber) resultInfo.EndPointNumber = resultStr;
+                        }
+
+                        ExportResult.Add(resultInfo);
+                    }
+                }
+            }
+            this.Close();
+        }
+    }
+}
