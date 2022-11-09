39f2564900f6e924617f087680ba46bc966fc5c6
AustinYanyh <1241528539@qq.com>
2022-11-09 10:15:15 +0800

    增加MainWindow的cs文件

---
+++ WpfTestProject/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+using Microsoft.Practices.Prism.Commands;
+
+namespace WpfTestProject
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public DelegateCommand DoWork { get; set; }
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            DoWork = new DelegateCommand(dowork);
+            
+            this.DataContext = this;
+        }
+
+        private void dowork()
+        {
+            MessageBox.Show("是不是这个IDE运行速度更快了");
+        }
+    }
+}
